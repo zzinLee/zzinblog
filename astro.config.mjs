b18666429537e8bcf72821a952613a81e3011747
zzinLee <sujin951017@gmail.com>
2025-03-31 20:43:12 +0900

    🔧 Sitemap config 추가

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,9 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: import.meta.env.SITE_URL || "https://zzinlee.dev",
+  integrations: [sitemap()],
+});

1021d72730e9f8ab9e744b87ca3e985ed7f5eea4
zzinLee <sujin951017@gmail.com>
2025-04-06 17:15:15 +0900

    node version20, react plugin

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
+import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://zzinlee.dev",
+  integrations: [react()],
 });

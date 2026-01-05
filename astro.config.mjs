4129a85e4d4f3598de525f8809c50f52fb53a586
zzinLee <sujin951017@gmail.com>
2026-01-05 18:06:34 +0900

    config 설정 추가

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,27 @@
 // @ts-check
-
+import { defineConfig, fontProviders } from "astro/config";
 import react from "@astrojs/react";
-import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://zzinlee.dev",
   integrations: [react()],
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  experimental: {
+    fonts: [
+      {
+        provider: fontProviders.google(),
+        name: "Roboto",
+        cssVariable: "--font-roboto",
+      },
+      {
+        provider: fontProviders.google(),
+        name: "Noto Sans KR",
+        cssVariable: "--font-noto-sans-kr",
+      },
+    ],
+  },
 });
